797842d889b7e14ed3d279ed4e5f4a6b9330ab81
albquintin <albertoquintinnavarro@gmail.com>
2020-12-29 21:14:50 +0100

    Inicio Proyecto, clases acommodation y trip

---
+++ __manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "NaturalParks",
+
+    'summary': """Manage Natural Parks""",
+
+    'description': """
+        Module for CA to manage natural parks:
+            - visitors
+            - own staff
+            - acommodation
+    """,
+
+    'author': "Alberto Quintin",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'views/natural_parks.xml',
+    ],
+    # only loaded in demonstration mode
+    #'demo': [
+    #    'demo.xml',
+    #],
+}
