286272ffec9da6adb940acf970e8b51097b31195
albquintin <albertoquintinnavarro@gmail.com>
2021-01-17 00:05:13 +0100

    Botones en trip y visitor

--- models/trip.py
+++ models/trip.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api, exceptions
+from odoo.exceptions import ValidationError
 
 class Trip(models.Model):
     _name = 'naturalparks.trip'
@@ -11,9 +12,33 @@ class Trip(models.Model):
     natural_park_id = fields.Many2one('naturalparks.natural_park', string="Natural Park", required=True)
     acommodation_id = fields.Many2one('naturalparks.acommodation', ondelete='cascade', string="Acommodation Organizer", required=True)
     visitor_ids = fields.Many2many('naturalparks.visitor', string="Visitors")
+    state = fields.Selection([
+            ('draft', 'Draft'),
+            ('confirm', 'Confirm'),
+            ('done', 'Done'),
+        ], string='Status', readonly=True, default='draft')
 
     @api.constrains('natural_park_id', 'acommodation_id')
     def _check_acommodation_is_in_the_park(self):
         for r in self:
             if r.acommodation_id.natural_park_id != r.natural_park_id:
                 raise exceptions.ValidationError("The acommodation must be in the park")
+
+    def action_confirm(self):
+        for r in self:
+            r.state = 'confirm'
+            return {
+                'effect': {
+                    'fadeout': 'slow',
+                    'message': 'Trip Confirmed',
+                    'type': 'rainbow_man',
+                }
+            }
+            
+    def action_done(self):
+        for r in self:
+            r.state = 'done'
+
+    def action_draft(self):
+        for r in self:
+            r.state = 'draft'
--- models/visitor.py
+++ models/visitor.py
@@ -7,6 +7,30 @@ class Visitor(models.Model):
     name = fields.Char(required=True)
     address = fields.Char()
     job = fields.Char()
+    state = fields.Selection([
+            ('draft', 'Draft'),
+            ('confirm', 'Confirm'),
+            ('done', 'Done'),
+        ], string='Status', readonly=True, default='draft')
 
     acommodation_id = fields.Many2one('naturalparks.acommodation', ondelete='cascade', string="Acommodation", required=True)
     management_id = fields.Many2one('naturalparks.management', string="Person who registered this visitor")
+
+    def action_confirm(self):
+        for r in self:
+            r.state = 'confirm'
+            return {
+                'effect': {
+                    'fadeout': 'slow',
+                    'message': 'Visitor Confirmed',
+                    'type': 'rainbow_man',
+                }
+            }
+            
+    def action_done(self):
+        for r in self:
+            r.state = 'done'
+
+    def action_draft(self):
+        for r in self:
+            r.state = 'draft'
